fb475d29197dea5946532308b577b9b5001de0b7
iamhridoy11 <m_rahman_hridoy@yahoo.com>
2024-03-20 15:30:13 -0400

    magic

---
+++ magic-eight-balls/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import EightBall from './EightBall'
+import './App.css'
+
+export default function App() {
+  
+
+  return (
+    <>
+      <EightBall />
+    </>
+  )
+}
+
